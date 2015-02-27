1e2d0fddf23a72502486e717fee702f1f18ec6d0
Nick Bedford <email@nickbedford.com>
2015-02-27 15:18:14 +1000

    * Added support for nested data sources. * Added nested data source to test. * Added $.parentsOnly() selector function.

--- test/people.js
+++ test/people.js
@@ -3,6 +3,13 @@ function Person(name, age, bio)
 	this.Name = name;
 	this.Age = age;
 	this.Bio = bio;
+	this.Skills = [];
+}
+
+function Skill(title, description)
+{
+	this.Title = title;
+	this.Description = description;
 }
 
 var testPeople =
@@ -10,3 +17,6 @@ var testPeople =
 	new Person('Nick Bedford', 27, 'Nick is a photographer.'),
     new Person('Nathaniel Dash', 27, '<p>Nathaniel is a JavaScript programmer.</p><p>He also has a dog named Cooper.</p>')
 ];
+
+testPeople[0].Skills = [ new Skill('C#', 'C# .NET programming'), new Skill('PHP', 'PHP MVC programming') ];
+testPeople[1].Skills = [ new Skill('C++', 'C++ programming'), new Skill('JavaScript', 'JavaScript programming') ];
