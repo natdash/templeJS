e83f04f8ec727293169a607eedd4f44ae65e8361
Nick Bedford <github@nickbedford.com>
2015-02-27 13:56:45 +1000

    * Renamed temple namespaces to Title Case (Temple/TempleExtensions). * Removed requirement data-template (uses child template if so). * Gracefully fails when template is undefined (not sure if best practice). * Renamed TestData to Person.

---
+++ test/people.js
@@ -0,0 +1,12 @@
+function Person(name, age, bio)
+{
+	this.Name = name;
+	this.Age = age;
+	this.Bio = bio;
+}
+
+var testPeople =
+[
+	new Person('Nick Bedford', 27, 'Nick is a photographer.'),
+    new Person('Nathaniel Dash', 27, 'Nathaniel is a JavaScript programmer.')
+];
